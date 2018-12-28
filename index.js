cc9424440e055fd36b4b0ea4dd65af728e3d9c5d
Aneesh Venuturumilli <veeaneesh@gmail.com>
2018-12-28 03:28:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currentline,name){
   currentline.push(name);
-  var message = `Welcome, ${name}. You are number ${}`;
+  var message = `Welcome, ${name}. You are number ${currentline.length} in line.`;
 }
